433747f771b49ddd63bc255b5ec220b593303bd1
Mahmoud-Elbrer <111395110+Mahmoud-Elbrer@users.noreply.github.com>
2024-11-27 02:01:04 +0400

    sqlite app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import './home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rolodex',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor:  const Color(0xFF02BB9F),
+        primaryColorDark: const Color(0xFF167F67)
+      ),
+      home: const HomePage(title: 'Rolodex')
+    );
+  }
+}
